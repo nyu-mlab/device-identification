376d3a5030e5c03dda4c6cf896b6cac80fd8d9b2
Tom_Long <mytomlong@gmail.com>
2021-01-25 14:34:16 -0600

    add unsupervised

---
+++ src/cluster.py
@@ -0,0 +1,22 @@
+# Taiyu Long@mLab
+# 01/25/2021 
+
+
+# Design:
+# Node: name, times
+# connect nodes by distance
+# name a cluster with the name occurs most
+# time: O(n2), space: O(n)
+# implementation: Disjoint set union
+# times as ranking
+
+
+class Graph:
+    def find():
+    def union():
+
+
+
+class Node:
+    pass
+

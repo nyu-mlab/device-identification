9ce23399d64ad9fe3c63c4664fe548733b8a168d
Tom_Long <mytomlong@gmail.com>
2021-01-29 23:13:31 -0600

    finish cluster

--- src/cluster.py
+++ src/cluster.py
@@ -16,45 +16,68 @@
 
 
 import pandas as pd
-from collections import defaultdict
+from collections import defaultdict, Counter
 from editdistance import distance
+from pprint import pprint
 
-DATA_PATH = '../data/device.csv'
 DIST = 1
 
 class Graph:
     def __init__(self, data_path):
-        self.read_data(data_path)
         self.graph = dict() #string -> Node
-        self.parent = dict()
+        self.parent = dict() #string -> parent string
+        self.read_data(data_path)
+        self.display()
 
     def read_data(self, data_path):
         device_df = pd.read_csv(data_path).fillna('')
         fields = device_df.columns[1:5]
-        
+        for name in device_df[fields[2]]:
+            self.connect(name)
 
-    def find(self, node):
-        if node != self.parent[node]: self.parent[node] = self.find(self.parent[node]) 
-        return self.parent[node]
 
-    def union(self, node1, node2):
-        p1, p2 = self.find(node1), self.find(node2) 
-        if p1.times > p2.times:
+    def display(self):
+        cluster = defaultdict(list)
+        for name in self.parent: 
+            cluster[self.graph[self.find(name)]] += [self.graph[name]]
+        print("cluster number: ", len(cluster))
+        for k, v in cluster.items():
+            print("parent:", k.name, "times:", k.times)
+            for n in v:
+                print(n.name, n.times, end=',') 
+            print('\n')
+
+
+    def find(self, name):
+        if name != self.parent[name]:
+            self.parent[name] = self.find(self.parent[name]) 
+        return self.parent[name]
+
+
+    def union(self, name1, name2):
+        p1, p2 = self.find(name1), self.find(name2) 
+        if p1 == p2: return 
+        if self.graph[p1].times >= self.graph[p2].times:
             p1, p2 = p2, p1
         self.parent[p1] = p2
 
+
     def connect(self, name):
+        name = name.lower()
         if name in self.graph:
             self.graph[name].times += 1
+            parent = self.find(name)
+            self.parent[name] = name
+            self.union(name, parent) 
         else:
             newNode = Node(name)
             self.graph[name] = newNode
-            self.parent[newNode] = newNode
+            self.parent[name] = name 
             for name_ in self.graph:
                 if distance(name, name_) <= DIST:
                     #self.graph[name].neib += [newNode]
                     #newNode.neib += [self.graph[name]]
-                    self.union(newNode, self.graph[name_])
+                    self.union(name, name_)
                     return 
 
                     
@@ -62,8 +85,8 @@ class Graph:
 class Node:
     def __init__(self, name, times = 1):
         self.name = name
-        self.time = times
-        self.preprocess()
+        self.times = times
+        #self.preprocess()
         #self.neib = [] 
 
     def preprocess(self):
---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Taiyu Long@mLab
+# 01/29/2021 
+# main script to run the cluster 
+
+
+
+from cluster import * 
+
+DATA_PATH = '../data/device.csv'
+
+if __name__ == '__main__':
+   cluster = Graph(DATA_PATH) 
+
+    
